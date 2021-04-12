605517d5a2b3ea21d6f04828525c05b28b0f49ae
Roman Elizarov <elizarov@gmail.com>
2021-04-12 21:56:32 +0300

    Fixed rim geometry to maintain separation and shape on transforms

--- src/commonMain/kotlin/poly/FaceRim.kt
+++ src/commonMain/kotlin/poly/FaceRim.kt
@@ -1,32 +1,66 @@
 package polyhedra.common.poly
 
 import polyhedra.common.util.*
-import kotlin.math.*
-
-private const val MAX_RIM_FRACTION = 0.8
 
 class FaceRim(f: Face)  {
-    val evs = List(f.size) { i ->
-        val j = (i + 1) % f.size
-        (f[j] - f[i]).unit
-    }
+    val maxRim: Double
+    val rimDir: List<Vec3>
 
-    val rimDir = List(f.size) { i ->
-        val k = (i + f.size - 1) % f.size
-        val a = evs[i]
-        val b = evs[k]
-        val c = 1 - a * b
-        (a - b) / sqrt(2 * c - c * c)
-    }
-
-    val maxRim = run {
-        var maxRim = Double.POSITIVE_INFINITY
-        for (i in 0 until f.size) {
-            val j = (i + 1) % f.size
-            maxRim = minOf(maxRim, (f[j] - f[i]).norm / (evs[i] * rimDir[i] - evs[i] * rimDir[j]))
+    init {
+        val n = f.size
+        // edge vectors
+        val ev = List(n) { i ->
+            val j = (i + 1) % n
+            f[j] - f[i]
         }
-        maxRim *= MAX_RIM_FRACTION
-        maxRim
+        // edge unit vectors
+        val evu = List(n) { i ->
+            ev[i].unit
+        }
+        // angle bisectors
+        val bis = List(n) { i ->
+            val k = (i + n - 1) % n
+            evu[i] - evu[k]
+        }
+        // pseudo-incenter
+        val ic = run {
+            val sum = MutableVec3()
+            val tmp = MutableVec3()
+            for (i in 0 until n) {
+                val j = (i + 1) % n
+                val a1 = bis[i] * bis[i]
+                val b1 = -bis[i] * bis[j]
+                val c1 = bis[i] * ev[i]
+                val a2 = -b1
+                val b2 = bis[j] * bis[j]
+                val c2 = bis[j] * ev[i]
+                val d = det(a1, b1, a2, b2)
+                val t1 = det(c1, b1, c2, b2) / d
+                val t2 = det(a1, c1, a2, c2) / d
+                tmp.setToZero()
+                tmp += f[i]
+                tmp += bis[i] * t1
+                tmp += f[j]
+                tmp += bis[j] * t2
+                tmp /= 2.0
+                sum += tmp
+            }
+            sum /= n.toDouble()
+            sum
+        }
+        // vector from vertex to ic
+        val icd = List(n) { i ->
+            ic - f[i]
+        }
+        // shortest distance from ic to edge
+        maxRim = (0 until n).minOf { i ->
+            val j = (i + 1) % n
+            tangentDistance(icd[i], icd[j])
+        }
+        // rescale so that rimDir reaches ic when multiplied by maxRim
+        rimDir = List(n) { i ->
+            icd[i] / maxRim
+        }
     }
 
     val borderNorm = List(f.size) { i ->
